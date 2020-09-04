a8e6a257ce4f2c5434f8d7ea4bd60ee976d4c5c2
Antonio Placido <aplacido1992@gmail.com>
2020-09-04 17:26:32 -0400

    so dont forget the required segment as the readme stated, and remember to execute your handlers..

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,26 @@
-'use strict';
+"use strict";
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-const getSeats = async (req, res) => {};
+const getSeats = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("workshop_6");
+
+  const data = await db.collection("seats").find().toArray();
+  let seats = {};
+  data.forEach((seat) => {
+    seats[seat._id] = seat;
+  });
+  console.log(seats);
+  client.close();
+  res.status(200).json({ seats: seats, numOfRows: 8, seatsPerRow: 12 });
+  return seats;
+};
 
 module.exports = { getSeats };
--- server/routes.js
+++ server/routes.js
@@ -1,130 +1,85 @@
-const router = require("express").Router();
 const { MongoClient } = require("mongodb");
 const assert = require("assert");
+const router = require("express").Router();
+const { getSeats } = require("./handlers");
 
 require("dotenv").config();
-const { MONGO_URI } = process.env;
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-
-const NUM_OF_ROWS = 8;
-const SEATS_PER_ROW = 12;
-
-// Code that is generating the seats.
-// ----------------------------------
-const seats = {};
-const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
-for (let r = 0; r < row.length; r++) {
-  for (let s = 1; s < 13; s++) {
-    seats[`${row[r]}-${s}`] = {
-      _id: `${row[r]}-${s}`,
-      price: 225,
-      isBooked: false,
-    };
-  }
-}
-// ----------------------------------
-//////// HELPERS
-
-const exportSeats = Object.values(seats);
-const batchInsert = async () => {
-  try {
-    const client = await MongoClient(MONGO_URI, options);
-
-    await client.connect();
-
-    const db = client.db("workshop_6");
-
-    const r = await db.collection("seats").insertMany(exportSeats); //using the helper exportSeats on line 28
-
-    assert.equal(1, r.insertedCount);
-
-    client.close();
-  } catch ({ message }) {
-    console.log(message);
-  }
+const MONGO_URI = process.env.MONGO_URI;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-batchInsert();
+// const batchImport = async () => {
+//   try {
+//     const client = await MongoClient(MONGO_URI, options);
+
+//     await client.connect();
+
+//     const db = client.db("workshop_6");
+
+//     const r = await db.collection("seats").insertMany(exportSeats);
+
+//     assert.equal(1, r.insertedCount);
+
+//     client.close();
+//   } catch ({ message }) {
+//     console.log(message);
+//   }
+// };
+
+// batchImport();
+
+router.get("/api/seat-availability", getSeats);
 
 const getRowName = (rowIndex) => {
   return String.fromCharCode(65 + rowIndex);
 };
-
 const randomlyBookSeats = (num) => {
   const bookedSeats = {};
-
   while (num > 0) {
     const row = Math.floor(Math.random() * NUM_OF_ROWS);
     const seat = Math.floor(Math.random() * SEATS_PER_ROW);
-
     const seatId = `${getRowName(row)}-${seat + 1}`;
-
     bookedSeats[seatId] = true;
-
     num--;
   }
-
   return bookedSeats;
 };
-
 let state;
-
-router.get("/api/seat-availability", async (req, res) => {
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
-  return res.json({
-    seats: seats,
-    bookedSeats: state.bookedSeats,
-    numOfRows: 8,
-    seatsPerRow: 12,
-  });
-});
 
 let lastBookingAttemptSucceeded = false;
 
 router.post("/api/book-seat", async (req, res) => {
   const { seatId, creditCard, expiration } = req.body;
-
   if (!state) {
     state = {
       bookedSeats: randomlyBookSeats(30),
     };
   }
-
   const isAlreadyBooked = !!state.bookedSeats[seatId];
   if (isAlreadyBooked) {
     return res.status(400).json({
       message: "This seat has already been booked!",
     });
   }
-
   if (!creditCard || !expiration) {
     return res.status(400).json({
       status: 400,
       message: "Please provide credit card information!",
     });
   }
-
   if (lastBookingAttemptSucceeded) {
     lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
     return res.status(500).json({
       message: "An unknown error has occurred. Please try your request again.",
     });
   }
-
   lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
   state.bookedSeats[seatId] = true;
-
   return res.status(200).json({
     status: 200,
     success: true,
   });
 });
-
 module.exports = router;
